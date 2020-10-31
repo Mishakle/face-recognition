fe30ed01a8e64d3f755ea453df905cf3301ddd23
Mishakle <31828112+Mishakle@users.noreply.github.com>
2020-11-01 00:49:22 +0200

    upload

---
+++ src/particlesOption.js
@@ -0,0 +1,13 @@
+const particlesOptions = {
+    particles: {
+        number: {
+            value: 80,
+            density: {
+            enable: true,
+            value_area: 800
+            }
+        }
+    }
+}
+
+export default particlesOptions;
